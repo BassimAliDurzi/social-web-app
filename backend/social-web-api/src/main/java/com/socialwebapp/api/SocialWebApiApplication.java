5b9f4686581c29c573c0d1e3721f5048df2f6f7e
Bassim Durzi <bassim.ali.durzi@gmail.com>
2026-02-01 22:37:46 +0100

    Setup Backend Bootstrap

---
+++ backend/social-web-api/src/main/java/com/socialwebapp/api/SocialWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.socialwebapp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialWebApiApplication.class, args);
+    }
+
+}
---
+++ backend/social-web-api/src/test/java/com/socialwebapp/api/SocialWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialwebapp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
